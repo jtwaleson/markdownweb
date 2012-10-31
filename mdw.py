c69b9059db1e19b6f3203105c44928487f288b44
Jouke Thiemo Waleson <jouke@waleson.com>
2012-10-31 08:57:24 +0100

    Pep8 compliancy, also added a destination dir argument, which does not have to exist

--- mdw.py
+++ mdw.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import sys
 import yaml
 import markdown
 import os
@@ -8,21 +7,43 @@ import copy
 from Cheetah.Template import Template
 import shutil
 
-class readable_dir(argparse.Action):
-    def __call__(self,parser, namespace, values, option_string=None):
-        prospective_dir=values
-        if not os.path.isdir(prospective_dir):
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
-        if os.access(prospective_dir, os.R_OK):
-            setattr(namespace,self.dest,prospective_dir)
-        else:
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
 
+class writeable_dir(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        target_dir = os.path.abspath(values)
+        prospective_dir = target_dir
+        if not os.path.isdir(prospective_dir):
+            prospective_dir = os.path.dirname(prospective_dir)
+        if not os.path.isdir(prospective_dir):
+            raise argparse.ArgumentTypeError(
+               "writeable_dir:{0} is not a valid path".format(prospective_dir))
+        if os.access(prospective_dir, os.R_OK):
+            setattr(namespace, self.dest, target_dir)
+        else:
+            raise argparse.ArgumentTypeError(
+                "writeable_dir:{0} is not a writeable dir".format(
+                    prospective_dir))
+
+
+class readable_dir(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        prospective_dir = os.path.abspath(values)
+        if not os.path.isdir(prospective_dir):
+            raise argparse.ArgumentTypeError(
+                "readable_dir:{0} is not a valid path".format(prospective_dir))
+        if os.access(prospective_dir, os.R_OK):
+            setattr(namespace, self.dest, prospective_dir)
+        else:
+            raise argparse.ArgumentTypeError(
+                "readable_dir:{0} is not a readable dir".format(
+                    prospective_dir))
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('input_dir', nargs='?', action=readable_dir, default=os.getcwd())
+parser.add_argument('input_dir',  action=readable_dir,  default=os.getcwd())
+parser.add_argument('output_dir', action=writeable_dir, default=os.getcwd())
 args = parser.parse_args()
+
 
 def read_dir_config(directory, parent_config):
     new_config = copy.deepcopy(parent_config)
@@ -36,6 +57,7 @@ def read_dir_config(directory, parent_config):
                 new_config[key] = (directory, value)
     return new_config
 
+
 def get_template(config):
     tmpl = None
     if 'template' in config:
@@ -47,14 +69,14 @@ def get_template(config):
         tmpl = Template("$content")
     return tmpl
 
+
 def get_config_and_template(directory, config):
     config = read_dir_config(directory, config)
     template = get_template(config)
     return (config, template)
 
+
 def process_dir(indir, outdir, config={}):
-    indir = os.path.abspath(indir)
-    outdir = os.path.abspath(outdir)
 
     if not os.path.isdir(outdir):
         os.mkdir(outdir)
@@ -77,10 +99,7 @@ def process_dir(indir, outdir, config={}):
         else:
             shutil.copyfile(path, outpath)
 
+indir = os.path.abspath(args.input_dir)
+outdir = os.path.abspath(args.output_dir)
 
-
-
-#    for (path, dirs, files) in os.listdir(directory):
-#        print path, dirs, files
-
-process_dir(args.input_dir, '/home/jouke/Desktop/blaat')
+process_dir(indir, outdir)
